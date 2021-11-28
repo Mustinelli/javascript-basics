8f09c4598d36a40242fa2c77a9d0ceb509ab039d
Mustafa Abdillahi <mustafa.abdi1@outlook.com>
2021-11-28 20:53:06 +0000

    completed array test

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,79 @@
 const getNthElement = (index, array) => {
-  // your code here
+  return array[index % array.length];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(f => f.toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  return strings.map(f =>
+    f
+      .split('')
+      .reverse()
+      .join('')
+  );
 };
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(f => f % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = array.slice();
+  newArray.splice(index, 1);
+  return newArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  const vowelCheck = /^[aeiouAEIOU]/i;
+  const newArray = strings.filter(Strings => vowelCheck.test(Strings));
+  return newArray;
 };
 
 const removeSpaces = string => {
-  // your code here
+  return string.split(' ').join('');
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  let total = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    total += numbers[i];
+  }
+  return total;
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  strings.sort((a, b) => {
+    if (a.slice(-1) < b.slice(-1)) {
+      return 1;
+    }
+    return 0;
+  });
 };
 
 module.exports = {
